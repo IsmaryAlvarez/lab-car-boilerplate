6b4756a6db8cacfdaf330aa4449abdc404256abf
kuveee <kamilavillablanca23@gmail.com>
2018-01-02 12:26:33 -0300

    primer comentario

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,45 @@
 // La funcionalidad de tu proyecto
-$(document).ready(function(){
+/*$(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
-/*
-// Get the modal
-var modal = document.getElementById('id01');
+*/
+/**
+ * Función centrado de imagen en vista splash
+ */
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+$.fn.center = function () {
+  this.css("position","absolute");
+  this.css("top", Math.max(0, (
+    ($(window).height() - $(this).outerHeight()) / 2) + 
+  $(window).scrollTop()) + "px"
+  );
+  this.css("left", Math.max(0, (
+    ($(window).width() - $(this).outerWidth()) / 2) + 
+  $(window).scrollLeft()) + "px"
+  );
+  return this;
 }
+
+/**
+ * cargado de función vista splash
+ */
+$("#overlay").show();
+$("#overlay-content").show().center();
+
+setTimeout(function(){    
+  $("#overlay").fadeOut();
+}, 2000);
+
+/**
+ * Función circulo vista splash
+ */
+$('#circle').circleProgress({
+ value: 1,
+ fill: {gradient: [['#ffffff', .5], ['#f7f2f5', .5]], gradientAngle: Math.PI / 4}
+
+}).on('circle-animation-progress', function(event, progress, stepValue) {
+  $(this).find('strong').text(stepValue.toFixed(2).substr(1));
+  
+});
+
+
